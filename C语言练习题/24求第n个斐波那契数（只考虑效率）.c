a0f70321f24fce2af090c574aef1d86b102d7401
Megang <1280529925@qq.com>
2022-07-03 09:55:47 +0800

    入门

---
+++ C语言练习题/24求第n个斐波那契数（只考虑效率）.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int Fbe(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int ret = Fbe(n);
+	printf("%d\n", ret);
+	return 0;
+}
